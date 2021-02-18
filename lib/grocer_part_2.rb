c3f7fd6062aa72be86fcd1e858460e5b5118d5d1
Brian Cook <brybcook@gmail.com>
2021-02-18 18:40:51 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -45,4 +45,12 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  consolidated_cart=consolidate_cart(cart)
+  cart_with_coupons=apply_coupons(consolidated_cart, coupons)
+  final=apply_clearance(cart_with_coupons)
+  
+  total=0
+  final.each do |items|
+    total+=items[:price]*items[:count]
+  end
 end

359c04ce2bac27ef53a84d546826f59adb6acf9b
Brian Cook <brybcook@gmail.com>
2021-02-18 18:36:53 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -29,7 +29,7 @@ def apply_clearance(cart)
   # REMEMBER: This method **should** update cart
   cart.each do |items|
     if items[:clearance]
-      items[:price]*= .8
+      items[:price]*= 0.8
     end
   end
   cart
